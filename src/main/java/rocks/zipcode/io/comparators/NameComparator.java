7b206d5b8c6607a094058a0df2573b6c5211f32c
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-07-31 08:25:49 -0400

    Name COmparatopr Done

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -9,7 +9,18 @@ import java.util.Comparator;
  */
 public class NameComparator implements Comparator<Item> {
     @Override
-    public int compare(Item o1, Item o2) {
-        return 0;
+    public int compare(Item item1, Item item2) {
+        int ans = 0;
+
+        for (int i = 0; i < item1.getName().length(); i++) {
+                if ((item1.getName().charAt(i) == item2.getName().charAt(i))) {
+                    ans = 0;
+            } else if ((item1.getName().charAt(i) != item2.getName().charAt(i))) {
+                    ans = (item1.getName().charAt(i) - item2.getName().charAt(i));
+                    break;
+                }
+
+        }
+        return ans;
     }
 }
