93b7a513ec9b4e745dac3bd948456f25e9c1c7ab
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-07-31 08:15:54 -0400

    Id COmparator Done

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -1,33 +1,52 @@
 package rocks.zipcode.io;
 
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class Item {
+public class Item implements Comparator <Item> {
+
+    Long id = 0L;
+    String name = "";
+    Double price = 0.01;
     public Item() {
     }
 
     public Item(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Long getId() {
-        return null;
+        return this.id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public Double getPrice() {
-        return null;
+        return this.price;
     }
 
     public void setPrice(Double price) {
+        this.price = price;
     }
+
+    @Override
+    public int compare(Item o1, Item o2) {
+        return 0;
+    }
+
+
 }
--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -5,11 +5,30 @@ import java.util.Comparator;
 /**
  * @author leon on 30/01/2019.
  */
-public class ItemSorter {
+public class ItemSorter implements Comparator <Item> {
+
+    private Item[] items;
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
+        //Get the value of each item's ID
+        for (int i = 0; i < this.items.length; i++) {
+            for (int j = 0; j < this.items.length; j++) {
+                
+            }
+        }
+        //compare and sort them in ascending order
+
+
+
+
         return null;
+    }
+
+    @Override
+    public int compare(Item o1, Item o2) {
+        return 0;
     }
 }
